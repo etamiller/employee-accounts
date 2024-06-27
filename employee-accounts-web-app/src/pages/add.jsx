a6f91f04083f4d83a11747d0100029b7f76cebf7
Serach Schubert <serachschubert@gmail.com>
2024-06-27 16:03:41 -0400

    add employee page with bootstrap

--- employee-accounts-web-app/src/pages/add.jsx
+++ employee-accounts-web-app/src/pages/add.jsx
@@ -1,10 +1,123 @@
-import { Outlet} from "react-router-dom"
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
+import Alert from 'react-bootstrap/Alert';
 
 export function Add() {
-    return(
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  function createList() {
+    console.log('clicked')
+    return (
         <>
-            <h1>This is the add page</h1>
-           <Outlet> </Outlet> 
+          {[
+            'primary',
+            'secondary',
+            'success',
+            'danger',
+            'warning',
+            'info',
+            'light',
+            'dark',
+          ].map((variant) => (
+            <Alert key={variant} variant={variant}>
+              This is a {variant} alert—check it out!
+            </Alert>
+          ))}
         </>
-    )
+      );
+    }
+  
+
+  return (
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="my-3">
+        <Form.Group as={Col} md="6" controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Mark"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="6" controlId="validationCustom02">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Otto"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="5" controlId="validationCustom03">
+          <Form.Label>Address</Form.Label>
+          <Form.Control type="text" placeholder="1234 North Street" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid street address.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom04">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" placeholder="Baltimore" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="2" controlId="validationCustom05">
+          <Form.Label>State</Form.Label>
+          <Form.Control type="text" placeholder="MD" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid state.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="2" controlId="validationCustom06">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control type="text" placeholder="21215" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid zip.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row >
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom07">
+          <Form.Label>Phone number</Form.Label>
+          <Form.Control type="text" placeholder="012-345-6789" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Phone number.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom08">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="text" placeholder="markotto@gmail.com" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Email address.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom09">
+          <Form.Label>Position</Form.Label>
+          <Form.Control type="text" placeholder="Manager" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide the Emolyee's position.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row >
+      <Button type="submit" onClick={createList}>Add Employee</Button>
+    </Form>
+  );
 }
