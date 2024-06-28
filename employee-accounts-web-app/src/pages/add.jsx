df97d8cf416ce79adeb14c4f6834e2f5b89d4f7a
Serach Schubert <serachschubert@gmail.com>
2024-06-28 11:05:19 -0400

    revised

--- employee-accounts-web-app/src/pages/add.jsx
+++ employee-accounts-web-app/src/pages/add.jsx
@@ -87,6 +87,88 @@ export function Add() {
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
       </Row>
+      <Row className="mb-3">
+         <Form.Group as={Col} md="5" controlId="validationCustom03">
+           <Form.Label>Address</Form.Label>
+           <Form.Control type="text" 
+            placeholder="1234 North Street" 
+            required 
+            onChange={event => setAddress(event.target.value)}
+            />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid street address.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom04">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" 
+            placeholder="Baltimore" 
+            required 
+            onChange={event => setCity(event.target.value)}
+            />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="2" controlId="validationCustom05">
+          <Form.Label>State</Form.Label>
+          <Form.Control type="text" 
+            placeholder="MD" 
+            required 
+            onChange={event => setState(event.target.value)}
+            />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid state.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="2" controlId="validationCustom06">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control type="text" 
+          placeholder="21215" 
+          required 
+          onChange={event => setZip(event.target.value)}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid zip.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row >
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom07">
+          <Form.Label>Phone number</Form.Label>
+          <Form.Control type="text" 
+            placeholder="012-345-6789" 
+            required 
+            onChange={event => setPhone(event.target.value)}
+            />
+          <Form.Control.Feedback type="invalid">
+             Please provide a valid Phone number.
+           </Form.Control.Feedback>
+         </Form.Group>
+         <Form.Group as={Col} md="4" controlId="validationCustom08">
+           <Form.Label>Email</Form.Label>
+           <Form.Control type="text" 
+             placeholder="markotto@gmail.com" 
+             required 
+             onChange={event => setEmail(event.target.value)}
+             />
+           <Form.Control.Feedback type="invalid">
+             Please provide a valid Email address.
+           </Form.Control.Feedback>
+         </Form.Group>
+         <Form.Group as={Col} md="4" controlId="validationCustom09">
+           <Form.Label>Position</Form.Label>
+           <Form.Control type="text" 
+             placeholder="Manager" 
+             required 
+             onChange={event => setPosition(event.target.value)}
+             />
+           <Form.Control.Feedback type="invalid">
+             Please provide the Emolyee's position.
+           </Form.Control.Feedback>
+         </Form.Group>
+       </Row >
+
       {/* Other form groups (address, city, state, zip, phone, email, position) go here */}
       <Button type="submit">Submit Employee Information</Button>
       <Row className="mb-3">
